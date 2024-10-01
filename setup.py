2b1a2f657e246a710f4b9ec286232d46f43962ab
emax-wenjun <68534397+emax-wenjun@users.noreply.github.com>
2024-09-30 22:48:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from distutils.core import setup
-from Cython.Build import cythonize
 import numpy
 
 
@@ -7,5 +6,4 @@ setup(name='regev_python',
       description='A python implementation of the packed Regev encryption scheme',
       author='Jesko Dujmovic',
       version='0.1',
-      ext_modules=cythonize('serialize.pyx'),
       include_dirs=[numpy.get_include()])
